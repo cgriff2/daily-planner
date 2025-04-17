ac3e032c690624c474cde06e313bf5d1b256fb61
cgriff2 <103300567+cgriff2@users.noreply.github.com>
2025-04-17 16:52:00 -0600

    Fixing deployment error

--- src/main.tsx
+++ src/main.tsx
@@ -4,11 +4,14 @@ import './index.css'
 import App from './App.tsx'
 import { DndProvider } from 'react-dnd';
 import { HTML5Backend } from 'react-dnd-html5-backend';
+import { BrowserRouter } from 'react-router-dom';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <DndProvider backend={HTML5Backend}>
-      <App />
-    </DndProvider>
+    <BrowserRouter basename="/daily-planner">
+      <DndProvider backend={HTML5Backend}>
+        <App />
+      </DndProvider>
+    </BrowserRouter>
   </StrictMode>
 );
